155e0f8bba4acb442694143b5e0e0d7f18fe7a6a
IhebGarsii <122628933+IhebGarsii@users.noreply.github.com>
2023-07-27 11:21:51 +0100

    Delete AnimeStore/backend directory

--- AnimeStore/backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { login, register } = require("../controller/userController");
-const router = express.Router();
-
-//login
-router.post("/login", login);
-//signe up
-router.post("/signup", register);
-module.exports = router;
